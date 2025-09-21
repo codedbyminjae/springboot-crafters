a60f98cf5e1d3ddf68a1f697092bd7eb1c87d04b
codedbyminjae <minjaekim7111@naver.com>
2025-09-21 11:14:55 +0900

    Initial commit

---
+++ src/main/java/crafter/spring_crafter/SpringCrafterApplication.java
@@ -0,0 +1,13 @@
+package crafter.spring_crafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrafterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crafter/spring_crafter/SpringCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package crafter.spring_crafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
