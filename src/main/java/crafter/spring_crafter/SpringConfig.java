923b0fd2538cfa8558ba39eafdf2841719eeaadf
codedbyminjae <minjaekim7111@naver.com>
2025-09-26 01:06:25 +0900

    JDBC 연결 및 application.properties 수정

--- src/main/java/crafter/spring_crafter/SpringConfig.java
+++ src/main/java/crafter/spring_crafter/SpringConfig.java
@@ -1,15 +1,25 @@
 package crafter.spring_crafter;
 
 import crafter.spring_crafter.domain.Member;
+import crafter.spring_crafter.repository.JdbcMemberRepository;
 import crafter.spring_crafter.repository.MemberRepository;
 import crafter.spring_crafter.repository.MemoryMemberRepository;
 import crafter.spring_crafter.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     // 멤버 서비스랑 멤버 레포지토리를 빈으로 연결
     // 오토와이어드 느낌으로
     @Bean
@@ -19,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+         // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
